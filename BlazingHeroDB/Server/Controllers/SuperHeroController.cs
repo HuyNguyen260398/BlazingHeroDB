bbb2d57cb9945925b86dc481e3d8bae781a39fed
Huy Nguyen <huynguyen260398@gmail.com>
2021-11-20 09:36:16 +0700

    Added SuperHero Controller with Get metthod.

---
+++ BlazingHeroDB/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using BlazingHeroDB.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazingHeroDB.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        static List<Comic> comics = new()
+        {
+            new Comic() { Name = "Marvel"},
+            new Comic() { Name = "DC"}
+        };
+
+        List<SuperHero> heros = new()
+        {
+            new SuperHero() { FirstName = "Peter", LastName = "Parker", HeroName = "Spiderman", Comic = comics[0] },
+            new SuperHero() { FirstName = "Bruce", LastName = "Wayne", HeroName = "Batman", Comic = comics[1] },
+        };
+
+        public async Task<IActionResult> GetSuperHeros()
+        {
+            return Ok(heros);
+        }
+    }
+}

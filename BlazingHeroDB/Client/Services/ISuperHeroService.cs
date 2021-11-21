433c494a540a004cf9ea1e66b620c1c2a75716a0
Huy Nguyen <huynguyen260398@gmail.com>
2021-11-21 11:39:36 +0700

    Added SuperHeroDetail page.

--- BlazingHeroDB/Client/Services/ISuperHeroService.cs
+++ BlazingHeroDB/Client/Services/ISuperHeroService.cs
@@ -5,5 +5,6 @@ namespace BlazingHeroDB.Client.Services
     public interface ISuperHeroService
     {
         Task<List<SuperHero>> GetSuperHeroes();
+        Task<SuperHero> GetSuperHeroById(int id);
     }
 }
--- BlazingHeroDB/Client/Services/SuperHeroService.cs
+++ BlazingHeroDB/Client/Services/SuperHeroService.cs
@@ -12,6 +12,11 @@ namespace BlazingHeroDB.Client.Services
             this.httpClient = httpClient;
         }
 
+        public async Task<SuperHero> GetSuperHeroById(int id)
+        {
+            return await httpClient.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+        }
+
         public async Task<List<SuperHero>> GetSuperHeroes()
         {
             return await httpClient.GetFromJsonAsync<List<SuperHero>>("api/superhero");
